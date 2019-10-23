ac1c545c9b7be54dc67f17992db227a5e0e9d77b
luca.orlandini <luca.orlandini@docomodigital.com>
2019-10-23 15:49:34 +0200

    dynamic bounds by openstreetmap call

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const token = dataApi.access_token;
 const conf = JSON.parse(fs.readFileSync('config.json'));
 
 (geoCoordinates.geoByCityCoordinates('Livorno Italy').then(function(response){
-    const bounds = `${response.data[0].boundingbox[0]},${response.data[0].boundingbox[2]},${response.data[0].boundingbox[1]}`;
+    const bounds = `${response.data[0].boundingbox[0]},${response.data[0].boundingbox[2]},${response.data[0].boundingbox[1]},${response.data[0].boundingbox[3]}`;
     const activity_type = 'running';
-    axios.get(`${conf.domain}${conf.api_version}${conf.end_point.segments}?bounds=${bounds},${response.data[0].boundingbox[3]}&activity_type=${activity_type}`, {
+    axios.get(`${conf.domain}${conf.api_version}${conf.end_point.segments}?bounds=${bounds}&activity_type=${activity_type}`, {
         headers: {
           Authorization: `Bearer ${token}`
         }
       })
       .then(function(response){
-          console.log(response.data);
+          //console.log(response.data);
       })
       .catch(function(error){
           console.log(error);
