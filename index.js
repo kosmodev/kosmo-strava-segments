9c5d12f7902c851bb38fb3d95e1bed4d23d3330b
luca.orlandini <luca.orlandini@docomodigital.com>
2019-10-23 10:27:14 +0200

    dinamyc api call

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const axios = require("axios");
+const geoCoordinates = require("./getCoordinates");
 const fs = require('fs');
 let rawdata = fs.readFileSync('data.json');
-
 let dataApi = JSON.parse(rawdata);
 const token = dataApi.access_token;
-axios.get("https://www.strava.com/api/v3/segments/explore?bounds=43.016571,10.521398,43.158596,10.63233&activity_type=running", {
-  headers: {
-    Authorization: `Bearer ${token}`
-  }
-})
-.then(function(response){
-    console.log(response.data);
-})
-.catch(function(error){
-    console.log(error);
-});
+const conf = JSON.parse(fs.readFileSync('config.json'));
+
+(geoCoordinates.geoByCityCoordinates('Livorno Italy').then(function(response){
+    const bounds = `${response.data[0].boundingbox[0]},${response.data[0].boundingbox[2]},${response.data[0].boundingbox[1]}`;
+    const activity_type = 'running';
+    axios.get(`${conf.domain}${conf.api_version}${conf.end_point.segments}?bounds=${bounds},${response.data[0].boundingbox[3]}&activity_type=${activity_type}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(function(response){
+          console.log(response.data);
+      })
+      .catch(function(error){
+          console.log(error);
+      });
+
+}));
