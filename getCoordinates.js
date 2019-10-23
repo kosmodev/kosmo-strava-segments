4efffe3f121a01c6483795ea43bf931918f3790d
luca.orlandini <luca.orlandini@docomodigital.com>
2019-10-23 10:31:32 +0200

    simple call to openstreemap

---
+++ getCoordinates.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+const OPEN_STREET_MAP_DOMAIN = 'https://nominatim.openstreetmap.org/';
+const geoByCityCoordinates = async (q) => {
+    const params = new URLSearchParams({
+      q,
+      format: "json",
+      polygon: 1,
+      addressdetails: 1,
+      limit: 1,
+
+    });
+
+    const ENDPOINT = `${OPEN_STREET_MAP_DOMAIN}search?${params.toString()}`;
+    const location = await axios.get(ENDPOINT);
+
+    return location;
+  };
+
+  module.exports.geoByCityCoordinates = geoByCityCoordinates;
